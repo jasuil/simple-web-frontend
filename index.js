db54a4882c0a360be1d4c7548434bc419aee486a
home <jasuil@daum.net>
2020-12-05 23:20:55 +0900

    change static view

--- index.js
+++ index.js
@@ -6,21 +6,27 @@ var session = require('express-session');
 var fs = require("fs")
 const cors = require('cors');
 
-app.set('views', __dirname + '/views');
+//app.set('views', __dirname + '/views');
 
-app.set('view engine', 'ejs'); 
-app.engine('html', require('ejs').renderFile);
-
-var server = app.listen(3000, function(){ 
-console.log(`Example app listening at http://localhost:${port}`)
-});
+//app.set('view engine', 'ejs'); 
+//app.engine('html', require('ejs').renderFile);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+	doc = '<h2>nice to meet you</h2><br/>'
+	doc += '<a href ="/idList.html"> idList</a><br/>'
+	doc += '<a href ="/index.html"> index</a><br/>'
+	res.send(doc)
 })
 
 
-var users = require('./user-router'); 
-app.use('/user', users);
+
+var static_handler = express.static(__dirname + '/views/');
+app.use(static_handler);
+//var users = require('./user-router'); 
+//app.use('/user', users);
 app.use(cors());
+
+var server = app.listen(3000, function(){ 
+console.log(`app listening at http://localhost:${port}`)
+});
